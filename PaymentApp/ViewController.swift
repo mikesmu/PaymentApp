700d52a25620ddcb84d937127189e68d6431214a
Michal Smulski <michal.smulski@grandparade.co.uk>
2019-09-12 10:42:53 +0200

    Initial Commit

---
+++ PaymentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by Michal Smulski on 12/09/2019.
+//  Copyright © 2019 msmulski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
